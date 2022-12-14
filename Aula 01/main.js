642442bd95e366b15888cec5c896e7a43de1596a
MarlonAlexAndrade <marlonalexandrade@gmail.com>
2022-12-13 22:00:53 -0300

    Aula 01

--- Aula 01/main.js
+++ Aula 01/main.js
@@ -19,5 +19,49 @@ preferencias.push("Python");
 console.table(preferencias);
 console.log(`Tipo de variavel prefencias ${typeof (preferencias)}`);
 console.log(`Meu nome é ${nome_completo} trabalho como ${profissao} goste de ${preferencias.join()}`);
-function exemplo() {
+function procedimento() {
 }
+// pode ser usado any
+function retorna10() {
+    return "10";
+}
+console.log(`A funcao retornou ${retorna10()}`);
+let retorno = retorna10();
+console.log(`A varivel retorno aproveito a funcao e resultou em ${retorno}`);
+//Somar
+function somar(numeroA, numeroB) {
+    return numeroA + numeroB;
+}
+console.log(`Ao somar 5 com 6 deu ${somar(5, 6)}`);
+//Subtrair
+function subtrair(numeroA, numeroB) {
+    return numeroA - numeroB;
+}
+console.log(`Ao subtrair 10 com 6 deu ${subtrair(10, 6)}`);
+//Multiplicar
+function multiplicar(numeroA, numeroB) {
+    return numeroA * numeroB;
+}
+console.log(`Ao multiplicar 10 com 10 deu ${multiplicar(10, 10)}`);
+//Divir
+function divir(numeroA, numeroB) {
+    return numeroA / numeroB;
+}
+console.log(`Ao divir 100 por 10 deu ${divir(100, 10)}`);
+function ehPar(numero) {
+    return numero % 2 == 0;
+    /* if (numero % 2 == 0) {
+         return "Par"
+    } else {
+         return "Impar"
+    } */
+    //Mesma funcao
+    // return (numero%2==0)?"Par":"Impar"
+}
+function salvarNoLocalStorage(valor, chave) {
+    localStorage.setItem(chave, valor);
+}
+function capturarValor(titulo) {
+    return prompt(`Informe o valor de ${titulo}`);
+}
+salvarNoLocalStorage(capturarValor("Chave do local storage"), capturarValor("Valor a ser salvo no local storage"));
--- Aula 01/main.ts
+++ Aula 01/main.ts
@@ -19,23 +19,94 @@ console.log(`%c Tipo de variavel ja_sei_ts ${typeof (ja_sei_ts)}`
 
 let nome_completo: string
 
-nome_completo="Marlon Alex Andrade"
+nome_completo = "Marlon Alex Andrade"
 
-idade=34
+idade = 34
 
 let profissao: string
 
-profissao="Vigilante"
+profissao = "Vigilante"
 //preferencia é um Array do tipo string vazio
-let preferencias:Array<string>=[]
+let preferencias: Array<string> = []
 
 preferencias.push("JavaScript")
 preferencias.push("TypeScritp")
 preferencias.push("Python")
 
-console.table(preferencias)
-console.log(`Tipo de variavel prefencias ${typeof(preferencias)}`);
+console.table(preferencias);
+
+console.log(`Tipo de variavel prefencias ${typeof (preferencias)}`);
 
 console.log(`Meu nome é ${nome_completo} trabalho como ${profissao} goste de ${preferencias.join()}`);
 
 
+
+
+function procedimento() {
+
+}
+// pode ser usado any
+function retorna10(): number | string | boolean {
+
+    return "10"
+}
+
+console.log(`A funcao retornou ${retorna10()}`);
+
+let retorno = retorna10();
+
+console.log(`A varivel retorno aproveito a funcao e resultou em ${retorno}`);
+
+//Somar
+function somar(numeroA: number, numeroB: number): number {
+    return numeroA + numeroB
+}
+
+console.log(`Ao somar 5 com 6 deu ${somar(5, 6)}`);
+
+//Subtrair
+function subtrair(numeroA: number, numeroB: number): number {
+    return numeroA - numeroB
+}
+
+console.log(`Ao subtrair 10 com 6 deu ${subtrair(10, 6)}`);
+
+//Multiplicar
+function multiplicar(numeroA: number, numeroB: number): number {
+    return numeroA * numeroB
+}
+
+console.log(`Ao multiplicar 10 com 10 deu ${multiplicar(10, 10)}`);
+
+//Divir
+function divir(numeroA: number, numeroB: number): number {
+    return numeroA / numeroB
+}
+
+console.log(`Ao divir 100 por 10 deu ${divir(100, 10)}`);
+
+function ehPar(numero: number):boolean {
+    
+        return numero%2==0
+    
+    /* if (numero % 2 == 0) {
+         return "Par"
+    } else {
+         return "Impar"
+    } */
+    //Mesma funcao
+   // return (numero%2==0)?"Par":"Impar"
+}
+
+function salvarNoLocalStorage(valor:string,chave:string):void{
+    localStorage.setItem(chave,valor)
+}
+
+function capturarValor(titulo:string):string{
+    return prompt(`Informe o valor de ${titulo}`)
+}
+
+salvarNoLocalStorage(
+    capturarValor("Chave do local storage"),
+    capturarValor("Valor a ser salvo no local storage")
+)
